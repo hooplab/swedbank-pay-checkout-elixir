91c9034d898907d15c513623519382a0648212c3
Lars Lone Rasmussen <lars@hoopla.no>
2020-10-19 17:10:11 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SwedbankpayCheckout.MixProject do
   def project do
     [
       app: :swedbankpay_checkout,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
